d2612a85564e9ff98398cc508befe6e311c969c2
Christiani <c.iradukunda@rwandaonline.rw>
2019-09-09 22:25:46 +0200

    implemented the api to return movies as list

--- src/app.js
+++ src/app.js
@@ -1,9 +1,34 @@
-const express =  require ('express');
+const express = require('express');
 const app = express();
 
+const movies = [{
+        id: 1,
+        name: 'movie1',
+        genre: 'adventure'
+    },
+    {
+        id: 2,
+        name: 'movie2',
+        genre: 'adventure'
+    },
+    {
+        id: 3,
+        name: 'movie3',
+        genre: 'adventure'
+    },
+    {
+        id: 4,
+        name: 'movie4',
+        genre: 'adventure'
+    }
+];
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('Welcome to vidly application');
-})
+});
+
+app.get('/movies', (req, res) => {
+    res.send(movies);
+});
 
 app.listen(3000, () => console.log('Listening on port 3000 ...'));
